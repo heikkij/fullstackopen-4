30a6642fd95e0da2cae3bc66c9f0ebbdfc7a5682
Heikki J <heikkij@rocketmail.com>
2018-05-05 14:39:30 +0300

    4.5* apufunktioita ja yksikkötestejä, osa 3

--- src/utils/list_helper.js
+++ src/utils/list_helper.js
@@ -1,7 +1,23 @@
-const totalLikes = (blogs) => {
+const totalLikes =  (blogs) => {
   return blogs.reduce(((acc, blog) => acc + blog.likes), 0)
+}
+
+const format = (blog) => {
+  if (blog) {
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes,
+    }
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  const [head, ...tail] = blogs
+  return tail.reduce((acc, blog) => blog.likes > acc.likes ? format(blog) : acc, format(head))
 }
 
 module.exports = {
   totalLikes,
+  favoriteBlog,
 }
