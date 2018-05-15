7318b10331e1844abb927de012ad7c4a50b632d4
Heikki J <heikkij@rocketmail.com>
2018-05-15 19:21:14 +0300

    Error handling fixed

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -5,24 +5,27 @@ const User = require('../models/user')
 const config = require('../utils/config')
 
 loginRouter.post('/', async (request, response) => {
-  const user = await User.findOne({ username: request.body.username })
-  const password = request.body.password
-  const loginCorrect = user !== null && password
-  console.log(password, user.passwordHash)
-  const authenticated = loginCorrect ? await bcrypt.compare(password, user.passwordHash) : false
+  try {
+    const user = await User.findOne({ username: request.body.username })
+    const password = request.body.password
+    const loginCorrect = user !== null && password
+    const authenticated = loginCorrect ? await bcrypt.compare(password, user.passwordHash) : false
 
-  if (!authenticated) {
+    if (!authenticated) {
+      throw Error()
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+
+    const token = jwt.sign(userForToken, config.secret)
+
+    response.status(200).send({ token, username: user.username, name: user.name })
+  } catch (exception) {
     return response.status(401).send({ error: 'invalid username or password' })
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id
-  }
-
-  const token = jwt.sign(userForToken, config.secret)
-
-  response.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
